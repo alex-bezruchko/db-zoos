c177d5da41512a7b1e76891bfa767d8d132010df
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-14 21:32:10 -0400

    added knex, sqlite libraries & added post api route

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+const db = [];
+
 // endpoints here
+
+server.post('/api/zoos', async (req,res) => {
+
+  const newAnimal = req.body;
+  try {
+    const added = db.insert(newAnimal);
+    if (added) {
+        res.status(201).json('Animal was added successfully.')
+    }
+    else {
+      res.json('Name and Description are required.')
+    }
+  }
+  catch (e) {
+    res.status(500).json(e);
+  }
+})
 
 const port = 3300;
 server.listen(port, function() {
